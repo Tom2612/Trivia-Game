c1afe4d3b41eade68a69d5b5efded8abb8e97c8e
Tom <tomcat-26@hotmail.co.uk>
2022-10-22 05:08:54 +0100

    added htmlfixer for special characters

--- src/QuestionItem.js
+++ src/QuestionItem.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import htmlFix from './htmlFixer';
 
 export default function QuestionItem(props) {
     const { question, allAnswers } = props.question;
 
     const answerElements = allAnswers.map(answer => (
         <h3 className='answer' onClick={() => props.selectAnswer(props.id, answer)}>
-            {answer.replace(/(&quot\;)/g,"\"").replace(/(&#039;)/g,"\'").replace(/(&amp;)/g,"\&")}
+            {htmlFix(answer)}
         </h3>
     ))
 
     return (
         <div id={props.id}>
-            <h3>{props.id + 1}) {question.replace(/(&quot\;)/g,"\"").replace(/(&#039;)/g,"\'").replace(/(&amp;)/g,"\&")}</h3>
+            <h3>{props.id + 1}) {htmlFix(question)}</h3>
             {answerElements}
         </div>
     )
